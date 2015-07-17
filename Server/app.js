f1e35491104e60a25601e638eaa05ef43c056f6f
SaintEmbers <quantum108@gmail.com>
2015-07-17 14:31:59 -0700

    login page

--- Server/app.js
+++ Server/app.js
@@ -9,7 +9,7 @@ var express = require('express');
 
 var app = express();
 var server = require('http').createServer(app);
-
+console.log('Jam City on port 8008')
 app.listen(8008);
 
 exports = module.exports = app;
