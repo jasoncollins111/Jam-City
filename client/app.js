c572ff12b1d0a95782b90cd9124d923040014ec6
SaintEmbers <quantum108@gmail.com>
2015-07-30 15:08:16 -0700

    services loaded

--- client/app.js
+++ client/app.js
@@ -1,7 +1,69 @@
-var app = angular.module('jamApp', [
-  'ngRoute'
+angular.module('jamApp', [
+  'jamApp.services'
 ])
 
+
+
+
+.controller('JamController', function ($scope, $location, CityInfo) {
+
+  $scope.eventName = [];
+
+  $scope.submit = function() {
+    if($scope.text) {
+      city = this.text;
+      getCityInfo.getCityId(city)
+      $scope.text = '';
+    }
+  };
+
+
+  // function getSpotifyIds(artist){
+  //   $http
+  //     .jsonp("http://developer.echonest.com/api/v4/artist/profile?api_key=APRGVYHQGMQ5FKTYM&id=songkick:artist:6833469&bucket=id:spotify&format=jsonp&callback=JSON_CALLBACK")
+  //     .success(function(data) {console.log(data)})
+  //     .error(function(data) {console.log(data)});
+  // }
+
+  // function createAnchors(artistObjArray){
+  //   var artist = artistObjArray[0]
+  // }
+
+
+
+});
+//   function getCityId(city){
+//     $.getJSON("http://api.songkick.com/api/3.0/search/locations.json?query="+city+"&apikey="+songkickKey+"&jsoncallback=?",
+//       function(data){
+//         // data is JSON response object
+//         var cityId = data.resultsPage.results.location[0].metroArea.id
+//         console.log(cityId)
+//         getEvents(cityId)
+//       });
+//   }
+//   function getEvents(cityId){
+//     $.getJSON("https://api.songkick.com/api/3.0/metro_areas/"+cityId+"/calendar.json?apikey="+songkickKey+"&jsoncallback=?",
+//       function(data){
+//         var events = data.resultsPage.results.event
+//         var eventArray = $scope.eventName
+//         for(var i = 0; i < events.length; i++){
+//           // var artist = events[i].performance[0].artist.id
+//           eventArray.push(events[i])
+//         }
+//         console.log('events:', $scope.eventName)
+//         for(var i = 0; i < eventArray.length; i++){
+//           var artist = eventArray[0].performance[0].artist.id
+
+//         }
+//           getSpotifyIds(artist)
+//       })
+//   };
+// .config(['$httpProvider', function($httpProvider) {
+
+//         $httpProvider.defaults.useXDomain = true;
+//         delete $httpProvider.defaults.headers.common['X-Requested-With'];
+//     }
+// ]);
 // app.config(['$routeProvider',
 //   function($routeProvider) {
 //     $routeProvider.
@@ -20,74 +82,3 @@ var app = angular.module('jamApp', [
 
 //   }
 // ])
-
-app.config(['$httpProvider', function($httpProvider) {
-
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-]);
-
-
-app.controller('JamController', ['$scope','$http', function($scope,$http) {
-  var echokey = 'APRGVYHQGMQ5FKTYM'
-  var consumerKey = 'ce78d10e8183380fb57357cc8a07e29d'
-  var echoSharedSecret = 'YhNZOH5TRUWGMogv/2XCZw'
-  var songkickKey = 'ngIhxhYsLMjkEU8y'
-  var city;
-  $scope.eventName = [];
-
-  $scope.submit = function() {
-    if($scope.text) {
-      city = this.text;
-      getCityId(city)
-      $scope.text = '';
-    }
-  };
-
-  function getEvents(cityId){
-    $.getJSON("https://api.songkick.com/api/3.0/metro_areas/"+cityId+"/calendar.json?apikey="+songkickKey+"&jsoncallback=?",
-      function(data){
-        var events = data.resultsPage.results.event
-        var eventArray = $scope.eventName
-        for(var i = 0; i < events.length; i++){
-          // var artist = events[i].performance[0].artist.id
-          eventArray.push(events[i])
-        }
-        console.log('events:', $scope.eventName)
-        for(var i = 0; i < eventArray.length; i++){
-          var artist = eventArray[0].performance[0].artist.id
-
-        }
-          getSpotifyIds(artist)
-      })
-  };
-
-  function getCityId(city){
-    $.getJSON("http://api.songkick.com/api/3.0/search/locations.json?query="+city+"&apikey="+songkickKey+"&jsoncallback=?",
-      function(data){
-        // data is JSON response object
-        var cityId = data.resultsPage.results.location[0].metroArea.id
-        console.log(cityId)
-        getEvents(cityId)
-      });
-  }
-  //"http://developer.echonest.com/api/v4/artist?api_key=APRGVYHQGMQ5FKTYM&artist=songkick:artist:6833469&format=jsonp&bucket=id:musicbrainz&callback=JSON_CALLBACK"
-  // var url = "http://developer.echonest.com/api/v4/artist/search?api_key=APRGVYHQGMQ5FKTYM&id=songkick:artist:6833469&format=jsonp&bucket=id:spotify&callback=JSON_CALLBACK"
-  function getSpotifyIds(artist){
-    $http
-      //.jsonp("http://developer.echonest.com/api/v4/artist/similar?api_key=APRGVYHQGMQ5FKTYM&artist=songkick:artist:6833469&format=jsonp&callback=JSON_CALLBACK")
-      //.jsonp("http://developer.echonest.com/api/v4/song/search?api_key=APRGVYHQGMQ5FKTYM&artist=songkick:artist:6833469&format=jsonp&callback=JSON_CALLBACK")
-      .jsonp("http://developer.echonest.com/api/v4/artist/profile?api_key=APRGVYHQGMQ5FKTYM&id=songkick:artist:6833469&bucket=id:spotify&format=jsonp&callback=JSON_CALLBACK")
-      //.jsonp("http://developer.echonest.com/api/v4/artist/news?api_key=APRGVYHQGMQ5FKTYM&id=spotify:artist:5l8VQNuIg0turYE1VtM9zV&format=jsonp&callback=JSON_CALLBACK")
-      .success(function(data) {console.log(data)})
-      .error(function(data) {console.log(data)});
-  }
-
-  function createAnchors(artistObjArray){
-    var artist = artistObjArray[0]
-  }
-
-
-
-}]);
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,29 +1,40 @@
 
-angular.module('shortly.services', [])
+angular.module('jamApp.services', [])
 
-.factory('getArtists', function ($http) {
+ // var echokey = 'APRGVYHQGMQ5FKTYM'
+ // var consumerKey = 'ce78d10e8183380fb57357cc8a07e29d'
+ // var echoSharedSecret = 'YhNZOH5TRUWGMogv/2XCZw'
+ // var songkickKey = 'ngIhxhYsLMjkEU8y'
+ // var city;
+
+.factory('CityInfo', function ($http) {
   // Your code here
   /* START SOLUTION */
-  var getCityId = function () {
-    return $http({
-      method: 'GET',
-      url: '/api/links'
-    })
-    .then(function (resp) {
-      return resp.data;
-    });
+
+  var getCityId = function (city) {
+  // //$.getJSON("http://api.songkick.com/api/3.0/search/locations.json?query="+city+"&apikey="+songkickKey+"&jsoncallback=?",
+  //   return $http({
+  //     method: 'GET',
+  //     url: "http://api.songkick.com/api/3.0/search/locations.json?query="+city+"&apikey="+songkickKey+"&jsoncallback=?"
+  //   .then(function (resp) {
+  //     console.log(resp)
+  //     return resp.data;
+  //   });
+  };
+  var getCityEvents = function (cityId) {
+
+  //   $.getJSON("https://api.songkick.com/api/3.0/metro_areas/"+cityId+"/calendar.json?apikey="+songkickKey+"&jsoncallback=?",
+
+  //   return $http({
+  //     method: 'GET',
+  //     url: "https://api.songkick.com/api/3.0/metro_areas/"+cityId+"/calendar.json?apikey="+songkickKey+"&jsoncallback=?",
+  //     data: link
+  //   });
   };
 
-  var getEvents = function (link) {
-    return $http({
-      method: 'POST',
-      url: '/api/links',
-      data: link
-    });
-  };
 
   return {
-    getAll: getAll,
+    getCityEvents: getCityEvents,
     addLink: addLink
   };
   /* END SOLUTION */
