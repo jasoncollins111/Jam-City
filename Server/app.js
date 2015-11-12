abf0c8cf4b97786cb3e03ec67b1baa327d5233bd
Nayigiziki <josephnayigiziki@gmail.com>
2015-11-11 16:30:08 -0800

    Restructured dir

--- Server/app.js
+++ Server/app.js
@@ -3,15 +3,12 @@
  */
 
 'use strict';
-
 var express = require('express');
-
-
 var app = express();
-// require('./express.js')(app)
+var port = 8008;
+
 require('./spotifyAuth/login/spotify.js')(app, express);
 
-var server = require('http').createServer(app);
-console.log('Jam City on port 8008')
-app.listen(8008);
+console.log('Jam City on port ', port);
+app.listen(port);
 exports = module.exports = app;
