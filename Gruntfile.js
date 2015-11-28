817ca6ccb2b81c6ca195d3e2651b5605668e9643
Nayigiziki <josephnayigiziki@gmail.com>
2015-11-28 15:58:22 -0800

    added gruntfile and made the proper html and js optimizations

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,65 @@
 module.exports = function(grunt) {
-  // Do grunt-related things in here
+
+  var cssFiles = [
+  'client/css/main.css',
+  'client/bower_components/materialize/bin/materialize.css'
+  ];
+
+  var staticJSFiles = [
+  'client/bower_components/jquery/dist/jquery.js',
+  'client/bower_components/angular/angular.js',
+  'client/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+  'client/bower_components/materialize/bin/materialize.js'
+  ];
+
+  var nonStaticJSFiles = [
+  'client/jamController.js',
+  'client/app/services/services.js',
+  'client/app.js',
+  ];
+
+  var jsFiles = staticJSFiles.concat(nonStaticJSFiles);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        mangle: false
+      },
+      bundle: {
+        files: {
+          'client/dist/bundle.min.js': 'client/dist/bundle.js'
+        }
+      }
+    },
+    concat: {
+      options: {
+        separator: ';',
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      bundle: {
+        src: jsFiles,
+        dest: 'client/dist/bundle.js',
+      },
+      css : {
+        src: cssFiles,
+        dest: 'client/dist/bundle.css'
+      }
+    },
+    watch: {
+      files: jsFiles,
+      tasks: ['prod']
+    }
+  });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  
+  grunt.registerTask('prod', ['concat', 'uglify']);
+
 };
+
+
+
