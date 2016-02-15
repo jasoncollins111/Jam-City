b82b873596085cdaa98ce0ff0e4339127289ea2e
Nayigiziki <josephnayigiziki@gmail.com>
2016-02-14 21:09:59 -0800

    added soundcloud player

--- client/app/artists/artistsCtrl.js
+++ client/app/artists/artistsCtrl.js
@@ -1,124 +1,164 @@
 angular.module('jamApp.controllers')
-.controller('JamEventListCtrl',['$scope', '$state', 'ArtistInfo', 'VenueSearch', '$timeout', 'City', '$window', '$anchorScroll',function ($scope, $state, ArtistInfo, VenueSearch, $timeout, City, $window, $anchorScroll) {
-  $scope.obj = {loading : true};
-  $scope.eventsList = [];
-  $scope.artistAdded = false
+    .controller('JamEventListCtrl', ['$scope', '$state', 'ArtistInfo', 'VenueSearch', '$timeout', 'City', '$window', '$anchorScroll', '$sce',
 
-  var getCityEventsIdThenUpdateEventsList = function(){
-      return City.getCity()
-      .then(function(city){
-        $scope.city = $scope.city ? $scope.city : city.displayName;
-        return City.getCityEvents(city.id);
-      })
-      .then(function(events){
-        console.log('in events', events);
-        return events.data.resultsPage.results.event;
-      })
-    .then(function(events){
-      displayEvents(events);
-      return true;
-    });
-  }
-
-  getCityEventsIdThenUpdateEventsList();
+        function($scope, $state, ArtistInfo, VenueSearch, $timeout, City, $window, $anchorScroll, $sce) {
+            $scope.obj = {
+                loading: true
+            };
+            $scope.eventsList = [];
+            $scope.artistAdded = false
+            var lastCallTimeStamp = null;
+            $scope.hideSongs = false;
 
 
-  $scope.getVenue = function(venueName){
-    $scope.artistClicked = {};
-    $state.go('artists')
-    $scope.obj = {loading : true};
-    var venueName = $scope.text
-    var venueId;
-    VenueSearch.venueId(venueName)
-    .then(function(res){
-      venueId = res.data.resultsPage.results.venue[0].id
-      return VenueSearch.venueEvents(venueId)
-    }).then(function(res){
-     console.log('venueEvents call returned',res)
-     var events = res.data.resultsPage.results.event
-     $scope.eventsList = []
-     searchEvents(events)
-   })
-  }
 
-  angular.element($window).bind("scroll", function(e) {
-    console.log('handling scroll event');
-    var windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
-    var body = document.body;
-    var html = document.documentElement;
-    var docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
-    windowBottom = windowHeight + window.pageYOffset;
+            $scope.$on('soundcloudSongs', function(event, songs) {
+                makePlaylist(songs);
+            });
 
-    if (windowBottom >= docHeight) {
-        console.log('getting some events!');
-        $timeout(function(){
-          getCityEventsIdThenUpdateEventsList()
-          .then(function(){
-            $anchorScroll.yOffset = 100;
-          });
-        }, 1500);
-    }
-  });
+            var makePlaylist = function(songs) {
+                $scope.songs = songs;
+                console.log($scope.songs);
+                $scope.$apply();
+            }
 
-  function displayEvents(events){
-    console.log('events ', events);
-    $scope.eventsList = $scope.eventsList ? $scope.eventsList : [];
-    var nameCache = {};
+            $scope.toggleClasses = function() {
+                $scope.hideSongs = !$scope.hideSongs;
+            }
+
+            $scope.playSong = function(song) {
+                $scope.stream_url = $sce.trustAsResourceUrl(song.stream_url + '?client_id=0272b27b7da270dd421f928fca6c9da0');
+            }
+
+            var getCityEventsIdThenUpdateEventsList = function() {
+                if (lastCallTimeStamp === null) {
+
+                } else {
+                    if (Date.now() - lastCallTimeStamp <= 60 * 1000) {
+                        return {
+                            then: function(callback) {
+                                console.log('not updating');
+                            }
+                        };
+                    }
+                }
+
+                lastCallTimeStamp = Date.now();
+                return City.getCity()
+                    .then(function(city) {
+                        $scope.city = $scope.city ? $scope.city : city.displayName;
+                        return City.getCityEvents(city.id);
+                    })
+                    .then(function(events) {
+                        console.log('in events', events);
+                        return events.data.resultsPage.results.event;
+                    })
+                    .then(function(events) {
+                        displayEvents(events);
+                        return true;
+                    });
+            }
+
+            getCityEventsIdThenUpdateEventsList();
 
 
-    for(var i = 0; i < events.length; i++){
-      if(events[i].performance.length > 0){
+            $scope.getVenue = function(venueName) {
+                $scope.artistClicked = {};
+                $state.go('artists')
+                $scope.obj = {
+                    loading: true
+                };
+                var venueName = $scope.text
+                var venueId;
+                VenueSearch.venueId(venueName)
+                    .then(function(res) {
+                        venueId = res.data.resultsPage.results.venue[0].id
+                        return VenueSearch.venueEvents(venueId)
+                    }).then(function(res) {
+                        console.log('venueEvents call returned', res)
+                        var events = res.data.resultsPage.results.event
+                        $scope.eventsList = []
+                        searchEvents(events)
+                    })
+            }
 
-        var artist = events[i].performance[0].artist.displayName;
-        var time = events[i].start.datetime !== null ? moment(events[i].start.datetime).calendar() : moment(events[i].start.date).calendar().split(' ')[0];
 
-        if(!nameCache[artist]){
-          $scope.eventsList.push({
-            artistName: artist,
-            artistId: events[i].performance[0].artist.id,
-            eventDateTime: {
-              date: time,
-              time: events[i].start.time
-            },
-            venue: events[i].venue.displayName,
-            venueId: events[i].venue.id
-            })
-          nameCache[artist] =  true;
+            angular.element($window).bind("scroll", function(e) {
+
+                var distanceFromTheTop = $(window).scrollTop();
+                var winHeight = $window.innerHeight;
+                var docHeight = $(document).height();
+
+                if (distanceFromTheTop + winHeight >= docHeight - 100) {
+                    $timeout(function() {
+                        getCityEventsIdThenUpdateEventsList()
+                            .then(function() {
+                                $anchorScroll.yOffset = 100;
+                            });
+                    }, 1500);
+                }
+            });
+
+            function displayEvents(events) {
+                console.log('events ', events);
+                $scope.eventsList = $scope.eventsList ? $scope.eventsList : [];
+                var nameCache = {};
+
+
+                for (var i = 0; i < events.length; i++) {
+                    if (events[i].performance.length > 0) {
+
+                        var artist = events[i].performance[0].artist.displayName;
+                        var time = events[i].start.datetime !== null ? moment(events[i].start.datetime).calendar() : moment(events[i].start.date).calendar().split(' ')[0];
+
+                        if (!nameCache[artist]) {
+                            $scope.eventsList.push({
+                                artistName: artist,
+                                artistId: events[i].performance[0].artist.id,
+                                eventDateTime: {
+                                    date: time,
+                                    time: events[i].start.time
+                                },
+                                venue: events[i].venue.displayName,
+                                venueId: events[i].venue.id
+                            })
+                            nameCache[artist] = true;
+                        }
+
+                    }
+                }
+                $scope.obj = {
+                    loading: false
+                };
+
+            }
+            $scope.artistDeets = function(artistClicked) {
+                var newId;
+                var artistId = artistClicked.artistId;
+
+                ArtistInfo.getSpotifyIds(artistId)
+                    .then(function(artistForeignId) {
+                        console.log('id ', artistForeignId)
+                        newId = artistForeignId.slice(15);
+                        return ArtistInfo.getInfo(newId);
+                    }).then(function(info) {
+                        if (info.status === 'found your pic bruh') {
+                            $scope.artistPic = info.image;
+                        }
+                        $scope.artistClicked = artistClicked;
+                        console.log('current state ', $state.current);
+                        if ($state.current.name !== 'artists.artist') {
+                            $state.go('artists.artist');
+                        }
+                    })
+                    .catch(function(err) {
+                        $scope.artistPic = 'http://cdn.playbuzz.com/cdn/71582f18-68a6-4ff0-942d-fd7090ffafd8/d56b4878-6ccb-4ce5-8101-f76d220a51d7.jpg';
+                        $scope.artistClicked = artistClicked;
+                        console.log('in ArtistDeets', artistClicked);
+                        if ($state.current.name !== 'artists.artist') {
+                            $state.go('artists.artist');
+                        }
+                    })
+            }
         }
-
-      }
-    }
-    $scope.obj = {loading : false};
-
-  }
-  $scope.artistDeets = function(artistClicked){
-    var newId;
-    var artistId  = artistClicked.artistId;
-
-    ArtistInfo.getSpotifyIds(artistId)
-    .then(function(artistForeignId){
-      console.log('id ', artistForeignId)
-      newId = artistForeignId.slice(15);
-      return ArtistInfo.getInfo(newId);
-    }).then(function(info){
-      if(info.status === 'found your pic bruh'){
-        $scope.artistPic = info.image;
-      }
-      $scope.artistClicked = artistClicked;
-      console.log('current state ', $state.current);
-      if ($state.current.name !== 'artists.artist') {
-        $state.go('artists.artist');
-      }
-    })
-    .catch(function(err){
-      $scope.artistPic = 'http://cdn.playbuzz.com/cdn/71582f18-68a6-4ff0-942d-fd7090ffafd8/d56b4878-6ccb-4ce5-8101-f76d220a51d7.jpg';
-      $scope.artistClicked = artistClicked;
-      console.log('in ArtistDeets', artistClicked);
-      if ($state.current.name !== 'artists.artist') {
-        $state.go('artists.artist');
-      }
-    })
-  }
-}]);
-
-
+    ]);
--- client/app/artists/artistsService.js
+++ client/app/artists/artistsService.js
@@ -2,74 +2,77 @@ var echokey = 'APRGVYHQGMQ5FKTYM';
 var consumerKey = 'ce78d10e8183380fb57357cc8a07e29d';
 var echoSharedSecret = 'YhNZOH5TRUWGMogv/2XCZw';
 var songkickKey = 'ngIhxhYsLMjkEU8y';
+
 angular.module('jamApp.services')
-.factory('City',['$http', '$q', function ($http, $q) {
-  var jammCityIdPromise;
-  var page = 1;
-  var getCityEvents = function (cityId) {
-    return $http.jsonp('https://api.songkick.com/api/3.0/metro_areas/'+cityId+'/calendar.json?apikey='+songkickKey+'&jsoncallback=JSON_CALLBACK&per_page=13&page='+ page++)
-  };
+    .factory('City', ['$http', '$q',
+        function($http, $q) {
+            var jammCityIdPromise;
+            var page = 1;
+            var getCityEvents = function(cityId) {
+                return $http.jsonp('https://api.songkick.com/api/3.0/metro_areas/' + cityId + '/calendar.json?apikey=' + songkickKey + '&jsoncallback=JSON_CALLBACK&per_page=20&page=' + page++)
+            };
 
-  var getCityEventsLatng = function(lat, lng){
-    return $http.jsonp('http://api.songkick.com/api/3.0/search/locations.json?location=geo:'+lat+','+lng+'&apikey='+songkickKey+'&jsoncallback=JSON_CALLBACK')
-  };
-
-
-  var getCityId = function(){
-    jammCityIdPromise = $q(function(resolve, reject){
-
-      navigator.geolocation.getCurrentPosition(function(position){
-        console.log('fixing to resolve promise');
-        resolve({
-          lat : position.coords.latitude,
-          lng : position.coords.longitude
-        })
-      }, function(err){
-        console.log('error getting position');
-        reject(err);
-      });
-
-    }).then(function(location){
-      return getCityEventsLatng(location.lat, location.lng);
-    }).then(function(cityInfo){
-      console.log('city Info', cityInfo);
-      var cityObj = {
-        id : cityInfo.data.resultsPage.results.location[0].metroArea.id,
-        displayName : cityInfo.data.resultsPage.results.location[0].metroArea.displayName
-      }
-      return cityObj;
-    });
-  }
+            var getCityEventsLatng = function(lat, lng) {
+                return $http.jsonp('http://api.songkick.com/api/3.0/search/locations.json?location=geo:' + lat + ',' + lng + '&apikey=' + songkickKey + '&jsoncallback=JSON_CALLBACK')
+            };
 
 
-  var getCity = function(){
-    return jammCityIdPromise;
-  }
+            var getCityId = function() {
+                jammCityIdPromise = $q(function(resolve, reject) {
 
-  return {
-    getCityEvents: getCityEvents,
-    getCityEventsLatng : getCityEventsLatng,
-    getCityId : getCityId,
-    getCity : getCity
-  };
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        console.log('fixing to resolve promise');
+                        resolve({
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        })
+                    }, function(err) {
+                        console.log('error getting position');
+                        reject(err);
+                    });
 
-}])
-.factory('VenueSearch', function($http){
-  function venueId(venueName){
-    return $http.jsonp('http://api.songkick.com/api/3.0/search/venues.json?query='+venueName+'&apikey='+songkickKey+'&jsoncallback=JSON_CALLBACK')
-    .success(function(data){
-      console.log(data)
+                }).then(function(location) {
+                    return getCityEventsLatng(location.lat, location.lng);
+                }).then(function(cityInfo) {
+                    console.log('city Info', cityInfo);
+                    var cityObj = {
+                        id: cityInfo.data.resultsPage.results.location[0].metroArea.id,
+                        displayName: cityInfo.data.resultsPage.results.location[0].metroArea.displayName
+                    }
+                    return cityObj;
+                });
+            }
+
+
+            var getCity = function() {
+                return jammCityIdPromise;
+            }
+
+            return {
+                getCityEvents: getCityEvents,
+                getCityEventsLatng: getCityEventsLatng,
+                getCityId: getCityId,
+                getCity: getCity
+            };
+
+        }
+    ])
+    .factory('VenueSearch', function($http) {
+        function venueId(venueName) {
+            return $http.jsonp('http://api.songkick.com/api/3.0/search/venues.json?query=' + venueName + '&apikey=' + songkickKey + '&jsoncallback=JSON_CALLBACK')
+                .success(function(data) {
+                    console.log(data)
+                })
+        }
+
+        function venueEvents(venueId) {
+            return $http.jsonp('http://api.songkick.com/api/3.0/venues/' + venueId + '/calendar.json?apikey=' + songkickKey + '&jsoncallback=JSON_CALLBACK')
+                .success(function(data) {
+                    console.log('venueEvents:', data)
+                })
+        }
+        return {
+            venueId: venueId,
+            venueEvents: venueEvents
+        }
     })
-  }
-  function venueEvents(venueId){
-   return $http.jsonp('http://api.songkick.com/api/3.0/venues/'+venueId+'/calendar.json?apikey='+songkickKey+'&jsoncallback=JSON_CALLBACK')
-   .success(function(data){
-    console.log('venueEvents:',data)
-  })
- }
- return{
-  venueId: venueId,
-  venueEvents: venueEvents
-}
-})
-
